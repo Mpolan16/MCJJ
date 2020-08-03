d87118b298fb1d0fcf70076a10f9cf9780b84ede
Caleb Jenkins <thecalebjenkins@gmail.com>
2020-08-03 19:31:52 -0400

    Initial commit with basic directory structure and starting to configure app.js and server.js

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Login from './components/login.js';
+import Home from './components/home.js';
+import Create from './components/create.js';
+import FavoritesList from './components/favoritesList.js';
+import NoMatch from "./pages/noMatch";
+import Footer from './components/footer.js';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router basename="/">
+        <div>
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/favorites/:id" component={FavoritesList} />
+            <Route exact path="/create" component={Create} />
+            <Route component={NoMatch} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
