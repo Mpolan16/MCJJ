eb66cd74eaa99feb85bb6976842537e2da961ec1
jdeleonardis <jdeleonardis@hotmail.com>
2020-08-06 19:28:21 -0400

    changed seeddb data and added some commented code to create.js

--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -1,4 +1,5 @@
 import React from "react";
+//import React, { useEffect } from "react";
 import SaveLib from "../components/saveLib";
 import Voice from "../components/voice";
 // import {Link} from "react-router-dom";
@@ -6,9 +7,19 @@ import Voice from "../components/voice";
 
 
 function Created() {
+
+    //   useEffect(() => {
+    //         // For demonstration purposes, we mock an API call.
+    //         API.getTemplateById("5f2c1a5cade96c1b044166ce").then((res) => {
+    //           //setDeveloperState(res);
+    //           //console.log("Developer State:");
+    //           console.log(res);
+    //         });
+    //       }, []);    
+
     return(
         <div>
-            <p>Generated Madlib</p>
+            <p>Generated Madlib</p>            
       
             {/* here we add the code for the generated madlib */}
             <SaveLib />
