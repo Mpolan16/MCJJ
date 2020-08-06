ca93aeb6c59b6be7a76b72214a5ed4c4386b5923
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-05 23:05:09 -0400

    created save lib component file and route for the create page

---
+++ client/src/components/saveLib.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SaveLib() {
+    return(
+        <div>
+            <p>list of saved libs here with a delete button on each list item</p>
+        </div>
+    );
+}
+
+export default SaveLib;
--- client/src/pages/create.js
+++ client/src/pages/create.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SaveLib from "../components/saveLib";
 // import {Link} from "react-router-dom";
 // import stories from "../utils/stories.json";
 
@@ -7,6 +8,7 @@ function Create() {
     return(
         <div>
             <p>Create</p>
+            <SaveLib />
             {/* story component(saved lib or new madlib buttons), saved libs component (delete button next to each saved item), voice component (make into class for component didmount (use effect too) inorder to get saved items from database, import json file), create a separate page for story selection form (which adds language, or category in future and THEN goes to Create(complete,generate) component) */}
         </div>
         
