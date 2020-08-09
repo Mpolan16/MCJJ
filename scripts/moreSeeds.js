35abf0ff9b4d27748b1db110cc4edbde132a36ac
Nikita <jrringler13759@gmail.com>
2020-08-08 22:23:14 -0400

    props to create and search by catLang

---
+++ scripts/moreSeeds.js
@@ -0,0 +1,18 @@
+
+
+[
+{
+    "title": "",
+    "story": "",
+    "prompts": [ ],
+    "category": "funny story",
+    "language": "English"
+},
+{
+    "title": "Vacations",
+    "story": "A vacation is when you take a trip to some ___ place with your ___ family. Usually you go to some place that is near a/an ___. A good vacation place is one where you can ride ___ or play ___ or go hunting for ___. I like to spend my time ___ or ___.",
+    "prompts": ["(1)adjective", "(2)adjective", "(3)noun", "(4)noun", "(5)plural noun", "(6)game", "(7)plural noun", "(8)verb ending in ING"],
+    "category": "funny story",
+    "language": "English"
+  }
+]
