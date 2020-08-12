64cea871e8f6fbb7bea89e49deb5e4540b6abdb9
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-11 22:59:16 -0400

    added the onchange function to signup form

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -27,6 +27,11 @@ class Firebase {
     this.auth.signInWithEmailAndPassword(email, password);
 
     doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+ 
+    doPasswordUpdate = password =>
+      this.auth.currentUser.updatePassword(password);
 }
  
 export default Firebase;
