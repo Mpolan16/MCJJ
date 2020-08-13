10742d2d0cda600c4732d84f74713c0f8570a090
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-13 19:28:53 -0400

    manage users with firebase raltime database

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -30,6 +30,10 @@ const NavigationAuth = () => (
       <Link to={ROUTES.ACCOUNT}>Account</Link>
     </li>
     <li>
+      <Link to={ROUTES.ADMIN}>Admin</Link>
+    </li>
+
+    <li>
       <SignOutButton />
     </li>
   </ul>
