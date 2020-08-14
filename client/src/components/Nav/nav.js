54b06ba9ca96b03fd6ace08ee424d0096b9ad693
Mónica Polanco <mpolan16@g.holycross.edu>
2020-08-13 22:45:53 -0400

    all folders imported correctly

--- client/src/components/Nav/nav.js
+++ client/src/components/Nav/nav.js
@@ -1,10 +1,10 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-import SignOutButton from './SignOut';
-import * as ROUTES from '../constants/routes';
+import SignOutButton from '../SignOut';
+import * as ROUTES from '../../constants/routes';
  
-import { AuthUserContext } from './Session';
+import { AuthUserContext } from '../Session';
  
 const Nav = () => (
   <div>
