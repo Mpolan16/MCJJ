386d2396d0c2fa29af16a69790b4639f698d033f
Caleb Jenkins <thecalebjenkins@gmail.com>
2020-08-18 18:30:07 -0400

    Finally pushing text to speech related files/code - also have added a fair amount of styling to createStory page in createStory.js

--- client/src/pages/CreateStory/createStory.js
+++ client/src/pages/CreateStory/createStory.js
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 // import SaveLib from "../components/saveLib";
 // import Voice from "../components/voice";
 import API from "../../utils/API.js";
-import Accents from "../../components/getAccents.js";
-import Read from "../../components/readStory.js";
-import {withRouter} from "react-router-dom";
+// import Accents from "../components/getAccents.js";
+// import Read from "../components/readStory.js";
+import SpeechContainer from "../../components/readStory.js"
+import { withRouter } from "react-router-dom";
+import './createStory.css';
 //import {Link} from "react-router-dom";  --use LINK if using to call another page
 
-const synth = window.speechSynthesis;
 
 function Create(props) {
     const [words, setWords] = useState(null)
@@ -15,28 +16,29 @@ function Create(props) {
     const [story, setStory] = useState({})
     const [userStory, setUserStory] = useState();
 
+    const [styledTemplate, setTemplate] = useState();
 
 
-    useEffect(() => {  
-        
-        console.log(props);
+
+    useEffect(() => {
+
+        // console.log(props);
         // console.log("Category",props.location.state.category)    
         // console.log("Language",props.location.state.language)    
 
-        // API.getTemplateById("5f2f5364fd5ba7031ccd93d9").then((res) => {
-        //     console.log(res);
-        //     //console.log(res.data[0].prompts);
+        API.getTemplateById("5f3b37adca4c3b3d844a3166").then((res) => {
 
-        //     // setStory({ details: res.data.story, title: res.data.story })
-        //     // setWords(res.data.prompts)
-        // });
-
-        API.getTemplatesByCategoryAndLanguage(props.location.state.cat, props.location.state.lang).then((res) => {
-            console.log(res);
-            console.log(res.data.prompts);
-             setStory({ details: res.data[0].story, title: res.data[0].title })
-             setWords(res.data[0].prompts)
+            setStory({ details: res.data.story, title: res.data.story })
+            setWords(res.data.prompts)
+            setTemplate(res.data.story)
         });
+
+        // API.getTemplatesByCategoryAndLanguage(props.location.state.cat, props.location.state.lang).then((res) => {
+        //     console.log(res);
+        //     console.log(res.data.prompts);
+        //     setStory({ details: res.data[0].story, title: res.data[0].title })
+        //     setWords(res.data[0].prompts)
+        // });
 
 
     }, []);
@@ -46,67 +48,55 @@ function Create(props) {
         const index = e.target.dataset.index
         userValues[index] = value
         setUserValues(userValues)
-        console.log(userValues)
     }
 
     const onSubmit = (e) => {
         // make sure user filled all inputs
-        // Object.keys(userValues).length === words.length
+        // if (!Object.keys(userValues).length === words.length) {
+        //     alert("Please fill in all values");
+        //     return;
+        // }
 
         let userStory = story.details;
+        let styledStory = story.details;
 
         for (let i = 0; i < words.length; i++) {
-            console.log({ userValues })
-            userStory = userStory.replace("___", userValues[i] || "___")
+            const placeholder = "(" + i + ")___";
+            if (!userValues[i]) {
+                userValues[i] = "blank"
+            }
+            userStory = userStory.replace(placeholder, userValues[i] || "___");
+            styledStory = styledStory.split(placeholder).join("<button class='inputWords' onclick='translate()'>" + userValues[i] + "</button>");
         }
 
+        setTemplate(styledStory)
         setUserStory(userStory);
-        Accents();
-        if (synth.onvoiceschanged !== undefined) {
-            synth.onvoiceschanged = Accents;
-        };
-    }
 
-    // rate.addEventListener("change", e => rateValue.textContent = rate.value)
-    // rate.addEventListener("change", e => rateValue.textContent = rate.value)
+    }
 
     return (
         <div>
-            <p>Generated Madlib</p>
             {
                 words && (
                     <>
                         {words.map((element, i) => {
-                            return <input data-index={i} placeholder={element} onChange={onChange} />
+                            return <input className="word-input" data-index={i} placeholder={element} onChange={onChange} />
                         })}
-                        <button onClick={() => onSubmit()}>Submit</button>
+                        <button onClick={() => onSubmit()} className="submit-btn">SUBMIT</button>
                     </>
                 )
             }
             {
                 userStory && (
-                    <>
-                        <p id="returnedStory">
-                            {userStory}
-                        </p>
-                        <button>Save</button>
-                        <button>New Template</button>
-                        <button onClick={() => Read(userStory)}>Read</button>
-                    </>
+                    <SpeechContainer
+                        story={userStory}
+                        styledStory={styledTemplate}
+
+                    />
+
                 )
             }
-            <label htmlFor="rate">Rate</label>
-            <div id="rate-value" className="badge">1</div>
-            <input type="range" id="rate" className="custom=range" min="0.5" max="2" defaultValue="1" step="0.1"></input>
 
-            <label htmlFor="pitch">Pitch</label>
-            <div id="pitch-value" className="badge">1</div>
-            <input type="range" id="pitch" className="custom=range" min="0" max="2" defaultValue="1" step="0.1"></input>
-
-            <div className="form-group">
-                <select id="voice-select" className="form-control" style={{ width: "500px" }}></select>
-            </div>
-            <button className="btn"></button>
 
         </div>
 
