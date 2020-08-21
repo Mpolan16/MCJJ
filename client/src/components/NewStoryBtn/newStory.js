de176357f0b274dbb4199017de447d7ec47ef1b7
Nikita <jrringler13759@gmail.com>
2020-08-20 20:14:19 -0400

    home into separate components

---
+++ client/src/components/NewStoryBtn/newStory.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import "./newStory.css";
+
+
+function NewStory() {
+    return(
+    <Button className = "newStoriesBtn"><Image className = "btnImage"  src = "../../Assets/newStory.png"/>
+        <p className = "btnText" >Fill In New Story</p>
+    </Button>
+    )
+}
+
+export default NewStory;
---
+++ client/src/components/WriteNewStoryBtn/writeNew.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import "./writeNew.css";
+
+
+function WriteNew() {
+    return(
+        <Button className = "newStoriesBtn"><Image className = "btnImage"  src = "../../Assets/newTemplate.png"/>
+            <p className = "btnText">Write Your Own Story</p>
+        </Button>
+
+    )
+}
+
+export default WriteNew;
